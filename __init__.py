13ca2f8278c6c5aae6130afa933eb933e370d035
Leo Behe <leobehe@gmail.com>
2025-07-15 11:47:00 -0400

    first commit; create skeleton of CanvasLoader node; add basic image output functionality

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for canvas."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """Leo Behe"""
+__email__ = "leobehe@gmail.com"
+__version__ = "0.0.1"
+
+from .src.canvas.nodes import NODE_CLASS_MAPPINGS
+from .src.canvas.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
